7d20d7222faa39f489e9de0a5b6d2e26566eb5c6
abolafiolopez <abolafiolopez@gmail.com>
2023-11-24 12:16:21 +0100

    Botones y páginas que eliminar y editar

--- Src/proyecto/base/views.py
+++ Src/proyecto/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Tarea
 
@@ -20,3 +20,16 @@ class CrearTarea(CreateView):
     model = Tarea
     fields = '__all__'
     success_url = reverse_lazy('tareas')
+
+
+class EditarTarea(UpdateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
+
+
+class EliminarTarea(DeleteView):
+    model = Tarea
+    context_object_name = 'tarea'
+    success_url = reverse_lazy('tareas')
+    template_name = 'base/eliminar_tarea.html'
